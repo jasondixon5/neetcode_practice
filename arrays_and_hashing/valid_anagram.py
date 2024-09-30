1dbb31b501f85be36fc7a60711e37d5977643a2d
Jason Dixon <jasondixonmail@gmail.com>
2024-09-30 14:39:06 -0700

    Practice reimpl of problem 'Valid Anagram'

--- arrays_and_hashing/valid_anagram.py
+++ arrays_and_hashing/valid_anagram.py
@@ -30,61 +30,39 @@ s and t consist of lowercase English letters.
 Follow up: What if the inputs contain Unicode characters? How would you adapt 
 your solution to such a case?
 """
+def is_valid_anagram(strs):
 
-def hashmap_solution(s, t):
-
-    # Anagram if each str has same chars and same count of those chars
-
-    if len(s) != len(s):
-        return False
-
-    s_char_map = {}
-    t_char_map = {}
-
-    for char in s:
-        if char in s_char_map:
-            s_char_map[char] += 1
-        else:
-            s_char_map[char] = 1
-
-    for char in t:
-        if char in t_char_map:
-            t_char_map[char] += 1
-        else:
-            t_char_map[char] = 1
-
-    return s_char_map == t_char_map
-
-def hashmap_solution_not_using_dict_equality(s, t):
-    # Alternate solution that does not use Python's dict eq functionality
-
-    if len(s) != len(t):
-        return False
+    # Anagram means using all characters of str s, can create str t (all same 
+    # chars and same quantity of chars)
     
-    s_char_map = {}
-    t_char_map = {}
+    s1, s2 = strs 
 
-    for char in s:
-        if char in s_char_map:
-            s_char_map[char] += 1
+    if len(s1) != len(s2):
+        return False
+
+    # Get character counts for each string
+    
+    s1_letter_counts = {}
+    for i in range(len(s1)):
+        char = s1[i]
+        if char in s1_letter_counts:
+            s1_letter_counts[char] += 1
         else:
-            s_char_map[char] = 1
+            s1_letter_counts[char] = 1
 
-    for char in t:
-        if char in t_char_map:
-            t_char_map[char] += 1
-        else:
-            t_char_map[char] = 1
+    from collections import defaultdict
+    s2_letter_counts = defaultdict(int)
+    for char in s2:
+        s2_letter_counts[char] += 1
+    
+    # Compare character counts for each string
 
-    for char in s_char_map:
-        if s_char_map[char] == t_char_map.get(char):
-            del t_char_map[char]
+    for char, char_count in s1_letter_counts.items():
+        if char_count != s2_letter_counts.get(char):
+            return False
 
-    # If dicts equal then second dict should be empty
-    return not t_char_map
+    return True
 
-# Anagram means using all characters of str s, can create str t (all same chars 
-# and same quantity of chars)
 def test_hashmap_solution() -> None:
 
     inputs = {
@@ -95,66 +73,7 @@ def test_hashmap_solution() -> None:
 
     for input, expected in inputs.items():
         
-        str1, str2 = input
-        sol = hashmap_solution(str1, str2)
+        sol = is_valid_anagram(input)
         assert sol == expected
 
-def test_hashmap_solution_not_using_dict_equality() -> None:
-    
-    inputs = {
-        ("anagram", "nagaram"): True,
-        ("rat", "car"): False,
-        ("loony", "yooln"): True,
-    }
-
-    for input, expected in inputs.items():
-        
-        str1, str2 = input
-        sol = hashmap_solution_not_using_dict_equality(str1, str2)
-        assert sol == expected
-        
-def counter_solution(str1: str, str2: str) -> bool:
-    """
-    Use Python's built-in Counter class to create a dictionary for each string of
-    the format {'character': count} and then compare the two dictionaries.
-    """
-    return Counter(str1) == Counter(str2)
-
-def test_counter_solution() -> None:
-
-    inputs = {
-        ("anagram", "nagaram"): True,
-        ("rat", "car"): False,
-        ("loony", "yooln"): True,
-    }
-
-    for input, expected in inputs.items():
-
-        str1, str2 = input
-        sol = counter_solution(str1, str2)
-        assert sol == expected
-        
-def sorted_solution(str1: str, str2: str) -> bool:
-    """
-    Sort the strings. If two strings have the same number of chars in the same
-    order they're identical.
-    Time: O(nlogn) 
-    Space: O(1) -- if don't count any extra space for sorting
-    """
-    return sorted(str1) == sorted(str2)
-
-def test_sorted_solution() -> None:
-
-    inputs = {
-        ("anagram", "nagaram"): True,
-        ("rat", "car"): False,
-        ("loony", "yooln"): True,
-    }
-
-    for input, expected in inputs.items():
-
-        str1, str2 = input
-        sol = sorted_solution(str1, str2)
-        
-        assert sol == expected
-  
+ 
