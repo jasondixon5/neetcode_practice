2f412409154980def883904d18b2c968220a04b3
Jason Dixon <jasondixonmail@gmail.com>
2024-10-09 06:54:02 -0700

    practice reimpl of min_stack. This version actually follows neetcode site solution of two data structures so that get_min runs in O(1) time. My orig sol ran in O(n) time because it used a var to keep track of smallest item

--- stack/min_stack.py
+++ stack/min_stack.py
@@ -34,6 +34,50 @@ pop, top and getMin will always be called on non-empty stacks.
 class MinStack:
 
     def __init__(self):
+
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val):
+        
+        self.stack.append(val)
+
+        # Decide what to add to min stack
+        if not self.min_stack:
+            min_val = val
+        else:
+            min_val = min(val, self.min_stack[-1])
+        
+        self.min_stack.append(min_val)
+
+    def pop(self):
+
+        # Intentionally avoiding Python list's pop() func 
+        
+        if self.stack:
+            del self.stack[-1]
+        
+        if self.min_stack:
+            del self.min_stack[-1]
+
+        return None 
+
+    def top(self):
+        if self.stack:
+            return self.stack[-1]
+        else:
+            return None
+
+    def get_min(self):
+
+        if self.min_stack:
+            return self.min_stack[-1]
+        else:
+            return None
+
+class MinStackOriginal:
+
+    def __init__(self):
         
         self.stack = []
         self.min_idx = None
@@ -105,7 +149,7 @@ def test_min_stack_min_is_first_item_added():
     stk.push(15)
     stk.push(25)
 
-    assert stk.min_idx == 0
+    assert stk.min_stack[-1] == 9
 
 def test_min_stack_min_is_second_item_added():
 
@@ -114,7 +158,7 @@ def test_min_stack_min_is_second_item_added():
     stk.push(15)
     stk.push(25)
 
-    assert stk.min_idx == 1
+    assert stk.min_stack[-1] == 15
 
 def test_min_stack_pop_and_min_reset():
     stk = MinStack()
@@ -124,7 +168,7 @@ def test_min_stack_pop_and_min_reset():
     stk.push(1)
     stk.pop()
 
-    assert stk.getMin() == 2 
+    assert stk.get_min() == 2 
 
 def test_min_stack_push():
     # TODO: Can this be improved? Partly repeats func test_min_stack_create_new_stack()
@@ -151,7 +195,7 @@ def test_min_stack_get_min_where_min_is_2():
     stk.push(20)
     stk.push(3)
     expected = 2
-    assert stk.getMin() == 2
+    assert stk.get_min() == 2
 
     return None
 
@@ -163,28 +207,13 @@ def test_min_stack_site_test_case():
     output.append(stk.push(1))
     output.append(stk.push(2))
     output.append(stk.push(0))
-    output.append(stk.getMin())
+    output.append(stk.get_min())
     output.append(stk.pop())
     output.append(stk.top())
-    output.append(stk.getMin())
+    output.append(stk.get_min())
 
-    # Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+    # Input: ["MinStack", "push", 1, "push", 2, "push", 0, "get_min", "pop", "top", "get_min"]
     # Removed initial null in site test case output 
     # Output: [null,null,null,0,null,2,1]
 
     assert output == [None, None, None, 0, None, 2, 1]
-"""
- [4, 5, 9, 2]
-    
-    min = 2, idx = 3
-    pop()
-    [4, 5, 9]
-    _minify 
-       min = 4, idx 0
-        5 > 4 so no change to min
-        9 > 4 so no change to min
-        sets min_idx to idx 0
-    
-    [3, 2, ]
-        
-"""
