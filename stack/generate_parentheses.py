6aafd432bff3e4865aa698a9c39df35f414fa9ac
Jason Dixon <jasondixonmail@gmail.com>
2024-10-22 15:05:12 -0700

    reimplement practice of problems 'generate parentheses' and 'reverse polish notation'

--- stack/generate_parentheses.py
+++ stack/generate_parentheses.py
@@ -1,8 +1,8 @@
 """
-Given n pairs of parentheses, write a function to generate all combinations of well-formed 
+Given n pairs of parentheses, write a function to generate all combinations of well-formed
 parentheses.
 
- 
+
 
 Example 1:
 
@@ -12,36 +12,37 @@ Example 2:
 
 Input: n = 1
 Output: ["()"]
- 
+
 
 Constraints:
 
 1 <= n <= 8
 """
 
+def backtrack(open_count, closed_count, n, stack, result):
+
+    if open_count == closed_count == n:
+        result.append("".join(stack))
+        return
+
+    if open_count < n:
+        stack.append("(")
+        backtrack(open_count + 1, closed_count, n, stack, result)
+        stack.pop()
+
+    if closed_count < open_count:
+        stack.append(")")
+        backtrack(open_count, closed_count + 1, n, stack, result)
+        stack.pop()
+
 def generate_parentheses(n):
-	
-	stack = []
-	result = []
 
-	def backtrack(open_paren_n, closed_paren_n):
+    stack = []
+    result = []
 
-		if open_paren_n == closed_paren_n == n:
-			result.append("".join(stack))
-			return
+    backtrack(0, 0, n, stack, result)
 
-		if open_paren_n < n:
-			stack.append("(")
-			backtrack(open_paren_n + 1, closed_paren_n)
-			stack.pop()
-
-		if closed_paren_n < open_paren_n:
-			stack.append(")")
-			backtrack(open_paren_n, closed_paren_n + 1)
-			stack.pop()
-
-	backtrack(0, 0)
-	return result
+    return result
 
 def test_generate_parentheses_n3():
 
@@ -58,4 +59,3 @@ def test_generate_parentheses_n1():
 	solution = generate_parentheses(n)
 
 	assert sorted(solution) == sorted(expected)
-
--- stack/reverse_polish_notation.py
+++ stack/reverse_polish_notation.py
@@ -23,9 +23,8 @@ tokens[i] is "+", "-", "*", or "/", or a string representing an integer in the r
 """
 def interpret_rpn(tokens):
 
-    # TODO: Problem with this approach is that RPN technically only does the
-    #       encountered operation on two numbers at a time and stores that
-    #       
+    # Note that RPN works oon two numbers at a time and stores that result
+
     operator_tokens = {
         '+': add,
         '/': floordiv,
@@ -33,27 +32,24 @@ def interpret_rpn(tokens):
         '-': sub,
     }
 
-    intermediate_values = []
+    stack = []
 
-    for i in range(len(tokens)):
+    for char in tokens:
 
-        if tokens[i] not in operator_tokens:
-            intermediate_values.append(int(tokens[i]))
-
+        if char not in operator_tokens:
+            stack.append(int(char))
         else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            func = operator_tokens[char]
+            stack.append(
+                func(operand1, operand2)
+            )
 
-            operand2 = intermediate_values.pop()
-            operand1 = intermediate_values.pop()
-
-            operation = operator_tokens.get(tokens[i])
-            val = operation(operand1, operand2)
-
-            intermediate_values.append(val)
-
-    return intermediate_values[0]
+    return stack[0]
 
 def interpret_rpn_orig(tokens):
-   
+
     operator_tokens = {
         '+': add,
         '/': floordiv,
@@ -66,19 +62,19 @@ def interpret_rpn_orig(tokens):
     for char in tokens:
         # If operator, take last two items from stack and operate on them
         if char in operator_tokens:
-            
+
             operand2 = stack.pop()
             operand1 = stack.pop()
             val = operator_tokens[char](operand1, operand2)
             stack.append(val)
-        
+
         else:
             stack.append(int(char))
 
     return stack[0]
 
 def test_interpret_rpn_plus_mult_sub():
-    
+
     tokens = ["1","2","+","3","*","4","-"]
     expected = 5
     sol = interpret_rpn(tokens)
@@ -100,4 +96,3 @@ def test_interpret_rpn_contains_twodigit_neg_number():
     sol = interpret_rpn(tokens)
 
     assert sol == expected
-
